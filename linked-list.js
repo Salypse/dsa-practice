00ad2a0c588732e10a27f25bde5dfcdb4621df8f
Salypse <84153483+Salypse@users.noreply.github.com>
2025-11-14 14:33:05 -0600

    Create append function that adds new node to end of linked list

--- linked-list.js
+++ linked-list.js
@@ -2,6 +2,23 @@ class LinkedList{
     constructor() {
         this.head = null;
     };
+
+    append(value) {
+        if (!this.head) {
+            this.head = new Node(value)
+        } else {
+            let currentNode = this.head;
+            while (true) {
+                if (currentNode.next != null) {
+                    currentNode = currentNode.next
+                } else {
+                    currentNode.next = new Node(value)
+                    break
+                }
+            }
+
+        }
+    }
 };
 
 class Node {
@@ -10,3 +27,10 @@ class Node {
         this.next = next;
     };
 };
+
+const testList = new LinkedList()
+
+testList.append("1")
+testList.append("2")
+
+console.log(testList)
