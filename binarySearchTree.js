b5191a0bfc11aeb18e9784acb65d0fd929f2d91c
Salypse <84153483+Salypse@users.noreply.github.com>
2025-11-19 13:16:17 -0600

    Initial structure of bst node and tree classes

---
+++ binarySearchTree.js
@@ -0,0 +1,17 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(arr) {
+        this.root = this.buildTree(arr)
+    }
+
+    buildTree(arr) {
+
+    }
+}
