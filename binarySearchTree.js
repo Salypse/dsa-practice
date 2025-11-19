56a585377f138a37543005f5238e60e0f42d0e9b
Salypse <84153483+Salypse@users.noreply.github.com>
2025-11-19 14:39:41 -0600

    Create buildTree function that takes in sorted arr and creates a bst using its elements

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,3 +1,5 @@
+import { mergeSort } from "./sorting/mergeSort.js";
+
 class Node {
     constructor(data) {
         this.data = data;
@@ -8,10 +10,23 @@ class Node {
 
 class Tree {
     constructor(arr) {
-        this.root = this.buildTree(arr)
+        this.uniqueArr = [...new Set(mergeSort(arr))]
+        this.root = this.buildTree(this.uniqueArr)
     }
 
     buildTree(arr) {
+        if (arr.length === 0) {
+            return null
+        } if (arr.length === 1) {
+            return new Node(arr)
+        }
 
+        const middle = Math.floor(arr.length / 2)
+        const root = new Node(arr[middle])
+
+        root.left = this.buildTree(arr.slice(0, middle))
+        root.right = this.buildTree(arr.slice(middle + 1))
+
+        return root
     }
 }
