ec033660d0c21ee073f9f87cfd165273ed62716c
Salypse <84153483+Salypse@users.noreply.github.com>
2025-11-20 13:46:54 -0600

    Create insert/delete functions that will add or remove the node with the given value

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -8,7 +8,7 @@ class Node {
     }
 }
 
-class Tree {
+export class Tree {
     constructor(arr) {
         this.uniqueArr = [...new Set(mergeSort(arr))]
         this.root = this.buildTree(this.uniqueArr)
@@ -28,5 +28,65 @@ class Tree {
         root.right = this.buildTree(arr.slice(middle + 1))
 
         return root
+    }
+
+    insert(value) {
+        if (this.root === null) {
+            this.root = new Node(value)
+            return
+        }
+
+        const insertRecursive = (node) => {
+            if (!node) {
+                return new Node(value)
+            }
+
+            if (value < node.data) {
+                node.left = insertRecursive(node.left)
+            } else if (value > node.data) {
+                node.right = insertRecursive(node.right)
+            } else {
+                //Returns if value is already in tree
+                return node
+            }
+            return node
+        }
+        this.root = insertRecursive(this.root)
+    }
+
+    delete(value) {
+        if (this.root === null) {
+            return
+        }
+
+        const deleteRecursive = (node, valueToRemove) => {
+            if (!node) {
+                return null
+            }
+
+            if (valueToRemove < node.data) {
+                node.left = deleteRecursive(node.left, valueToRemove)
+            } 
+            else if (valueToRemove > node.data) {
+                node.right = deleteRecursive(node.right, valueToRemove)
+            } else {
+                //0 or 1 child nodes
+                if (!node.left) return node.right
+                if (!node.right) return node.left
+                
+                //2 children nodes
+                else {
+                    let successor = node.right;
+                    while (successor.left) {
+                        successor = successor.left
+                    }
+
+                    node.data = successor.data
+                    node.right = deleteRecursive(node.right, successor.data)
+                }
+            }
+            return node
+        }
+        this.root = deleteRecursive(this.root, value)
     }
 }
